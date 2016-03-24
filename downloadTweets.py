12d77493bfc90d25bf60e00fe107ba10532c8485
proprefenetre <nielseigenraam@gmail.com>
2016-03-24 16:38:13 +0100

    turn the whole thing into a function

--- downloadTweets.py
+++ downloadTweets.py
@@ -11,7 +11,10 @@ import re
 from TweetStore import TweetStore
 from vectorization import Vectorizer
 
+
 URL = re.compile(r'(https?)?:\/\/(\w\.\w.)\/\w*')
+COUCH_SERVER = 'http://192.168.1.106:5984/'
+
 
 # fuction that flattens the dictionary structure
 def flatten(d, parent_key='', sep='_'):
@@ -24,6 +27,7 @@ def flatten(d, parent_key='', sep='_'):
             items.append((new_key, v))
     return dict(items)
 
+
 # function that strips out unnecessary stuff
 def prep_txt(txt):
     try:
@@ -33,12 +37,14 @@ def prep_txt(txt):
     except:
         pass
 
+
 # maps a series onto another series
 def createMap(k_series, v_series):
     keys = list(k_series)
     vals = list(v_series)
     
     return dict({(i,n) for i, n in zip(keys, vals)})
+
 
 # calculate the center of a bounding box of coordinates
 def center(b_box, rev=True):
@@ -52,42 +58,79 @@ def center(b_box, rev=True):
 
     return list(c)
 
-# connect to the server
-COUCH_SERVER = 'http://192.168.1.106:5984/'
-db = TweetStore('twitter_locations', COUCH_SERVER)
 
-# to speed things up a little bit, we can process the text later
-tweets = [flatten(dict(t)) for t in db.get_tweets()]
-
-df = pd.DataFrame(tweets)
-
-# select usefol columns
-df=df[['_id', 'created_at', 'lang', 'place_bounding_box_coordinates',
+def getDF(db_name, server):
+    db = TweetStore(db_name, server)
+    tweets = [flatten(dict(t)) for t in db.get_tweets()]
+    df = pd.DataFrame(tweets)
+    df=df[['_id', 'created_at', 'lang', 'place_bounding_box_coordinates',
        'place_country', 'place_country_code', 'place_full_name', 'place_id',
        'place_name', 'place_place_type', 'text', 'user_lang',
        'user_location']]
+    df = df.dropna(subset=["place_bounding_box_coordinates"])
+
+    df.text = df.text.apply(prep_txt)
+
+    en = df[df.user_lang == 'en']
+    n_en = df[df.user_lang != 'en']
+
+    en_idMap = createMap(en.place_id, en.place_name)
+    n_en_idMap = createMap(n_en.place_id, n_en.place_name)
+
+    for k,v in n_en_idMap.items():
+        if k not in en_idMap:
+            en_idMap[k] = v
+
+    df["place_name_tr"] = df["place_id"].map(en_idMap)
+
+    # calculate bbox centers:
+    df["place_center"] = df.place_bounding_box_coordinates.apply(center)
+
+    return df
+
+# db = TweetStore('twitter_locations', COUCH_SERVER)
+# tweets = [flatten(dict(t)) for t in db.get_tweets()]
+# df = pd.DataFrame(tweets)
+
+# select usefol columns
+# df=df[['_id', 'created_at', 'lang', 'place_bounding_box_coordinates',
+       # 'place_country', 'place_country_code', 'place_full_name', 'place_id',
+       # 'place_name', 'place_place_type', 'text', 'user_lang',
+       # 'user_location']]
 
 # drop tweets without location info
-df = df.dropna(subset=["place_bounding_box_coordinates"])
+# df = df.dropna(subset=["place_bounding_box_coordinates"])
 
 # transform the text data
-df.text = df.text.apply(prep_txt)
+# df.text = df.text.apply(prep_txt)
 
 # translate place names create 2 maps: one with the id's and place names for the 
 # english user i-face data, one for the non-english data. then update the first 
 # with the second.
-en = df[df.user_lang == 'en']
-n_en = df[df.user_lang != 'en']
+# en = df[df.user_lang == 'en']
+# n_en = df[df.user_lang != 'en']
 
-en_idMap = createMap(en.place_id, en.place_name)
-n_en_idMap = createMap(n_en.place_id, n_en.place_name)
+# en_idMap = createMap(en.place_id, en.place_name)
+# n_en_idMap = createMap(n_en.place_id, n_en.place_name)
 
-for k,v in n_en_idMap.items():
-    if k not in en_idMap:
-        en_idMap[k] = v
+# for k,v in n_en_idMap.items():
+#     if k not in en_idMap:
+#         en_idMap[k] = v
 
-df["place_name_tr"] = df["place_id"].map(en_idMap)
+# df["place_name_tr"] = df["place_id"].map(en_idMap)
+
+# # calculate bbox centers:
+# df["place_center"] = df.place_bounding_box_coordinates.apply(center)
+
+df1 = getDF('twitter_belgium_inc', COUCH_SERVER)
+df2 = getDF('twitter_locations', COUCH_SERVER)
+
 
 # write df to csv-file to speed things up
-with open("twitter_locations_data.csv", 'a') as f:
-    df.to_csv(f)
+with open("twitter_belgium.csv", 'w') as f1:
+    df1.to_csv(f)
+with open("twitter_locations.csv", 'w') as f2:
+    df2.to_csv(f)
+with open("twitter_complete.csv", 'w') as f3:
+    df = df1.append(df2)
+    df.to_csv(f3)
